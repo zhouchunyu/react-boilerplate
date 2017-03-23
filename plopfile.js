f57ccefe5b2fef72cf852f5c1dd363e837244e54
zhouchunyu <zhouchunyu@atyun.net>
2017-03-23 09:23:57 +0800

    finish plop

--- plopfile.js
+++ plopfile.js
@@ -1,37 +1,45 @@
 /**
  * Created by atyun on 17/3/20.
  */
+'use strict';
+const path = require('path');
 module.exports = function (plop) {
-    plop.addHelper('split', function (attrs) {
-        attrs = attrs.split(' ');
-        var attrs_s = "";
-        attrs.forEach((value) => {
-            attrs_s += "{title: '" + value + "', dataIndex: '" + value + "', key: '" + value + "'},"
-            });
-        return attrs_s;
-    });
     plop.addPrompt('recursive', require('inquirer-recursive'));
-    // resource generator
+
     plop.setGenerator('resource', {
-        description: 'an application resource',
-        prompts: [{
-            type: 'input',
-            name: 'name',
-            message: 'resource name please'
-        },{
-            type: 'recursive',
-            name: 'attrs',
-            message: 'add a attr?',
-            prompts: [{
-                    type: 'input',
-                    name: 'name',
-                    message: 'What is attr\'s name?',
-            }]
-        }],
-        actions: [{
-            type: 'add',
-            path: 'src/components/{{name}}.js',
-            templateFile: 'plop-templates/resource.txt'
-        }]
+        description: 'generate a resource',
+        prompts: [
+            {
+                type: 'input',
+                name: 'resource',
+                message: 'What is the resource name?',
+            }, {
+                type: 'recursive',
+                name: 'attrs',
+                message: 'Do you want to add an attr',
+                prompts: [
+                    {
+                        type: 'input',
+                        name: 'name',
+                        message: 'What is attr\'s name?',
+                    }
+                ]
+            }
+        ],
+        actions: [
+            {
+                type: 'add',
+                path: 'src/components/{{snakeCase resource}}/{{snakeCase resource}}.js',
+                templateFile: 'plop_template/resource.txt',
+                abortOnFail: true
+            },
+            {
+                type: 'modify',
+                path: 'src/components/App.js',
+                pattern: /{!!/gi,
+                templateFile: 'plop_template/route.txt'
+            }
+        ]
     });
+
 };
